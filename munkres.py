8d6c77139499be445fabf5d80141f28e5977a6c6
Brian Clapper <bmc@clapper.org>
2008-08-08 03:08:52 +0000

    Version -> 1.0.5.3

--- munkres.py
+++ munkres.py
@@ -290,7 +290,7 @@ __all__     = ['Munkres', 'make_cost_matrix']
 # ---------------------------------------------------------------------------
 
 # Info about the module
-__version__   = "1.0.5.2"
+__version__   = "1.0.5.3"
 __author__    = "Brian Clapper, bmc@clapper.org"
 __url__       = "http://www.clapper.org/software/python/munkres/"
 __copyright__ = "(c) 2008 Brian M. Clapper"
